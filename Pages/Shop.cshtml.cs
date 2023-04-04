0bd481fe29a06b8ae46ffb49ba600cc50328f162
diwyanjalamaytech <diwyanjala@maytech.au>
2023-04-04 18:03:50 +0530

    singla product

--- Pages/Shop.cshtml.cs
+++ Pages/Shop.cshtml.cs
@@ -1,19 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AD2_WEB_APP.Services;
+using AD2_WEB_APP.Models.ComputerModel;
 
 namespace AD2_WEB_APP.Pages;
 
 public class ShopModel : PageModel
 {
     private readonly ILogger<ShopModel> _logger;
-
-    public ShopModel(ILogger<ShopModel> logger)
+    private IComputerModelService _computerService;
+    public ShopModel(ILogger<ShopModel> logger,IComputerModelService computerService)
     {
         _logger = logger;
+        _computerService = computerService;
     }
-
+    public List<GetRequestComputerModel> itemList { get; set; }
     public void OnGet()
     {
-
+        itemList = _computerService.GetAll();
     }
 }
--- Pages/Single-product.cshtml.cs
+++ Pages/Single-product.cshtml.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AD2_WEB_APP.Services;
+using AD2_WEB_APP.Entities;
+using AD2_WEB_APP.Models.ComputerModel;
 
 namespace AD2_WEB_APP.Pages;
 
 public class SigleProductModel : PageModel
 {
     private readonly ILogger<SigleProductModel> _logger;
+    private IComputerModelService _computerService;
+     public GetRequestComputerModel item { get; set; }
 
-    public SigleProductModel(ILogger<SigleProductModel> logger)
+    public SigleProductModel(ILogger<SigleProductModel> logger,IComputerModelService computerService)
     {
         _logger = logger;
+        _computerService = computerService;
     }
 
     public void OnGet()
     {
-
+        item = _computerService.GetComputerById(1);
     }
 }
