6374141424e2924bd9370b342976ba8a3fe63fda
diwyanjalamaytech <diwyanjala@maytech.au>
2023-04-05 15:25:55 +0530

    added cart item

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using AD2_WEB_APP.Services;
 using AD2_WEB_APP.Models.ComputerModel;
+using Microsoft.AspNetCore.Http;
 namespace AD2_WEB_APP.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private IComputerModelService _computerService;
-    public IndexModel(ILogger<IndexModel> logger,IComputerModelService computerService)
+    public IndexModel(ILogger<IndexModel> logger, IComputerModelService computerService)
     {
         _logger = logger;
         _computerService = computerService;
@@ -16,6 +17,25 @@ public class IndexModel : PageModel
     public List<GetRequestComputerModel> itemList { get; set; }
     public void OnGet()
     {
+
         itemList = _computerService.GetAll();
     }
+
+
+    public void OnPostSetItem(int id,int price)
+    {
+        try
+        {
+            itemList = _computerService.GetAll();
+            HttpContext.Session.SetInt32("id" + id, id);
+            HttpContext.Session.SetInt32("price" + id, price);
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }
+
+    }
+
 }
